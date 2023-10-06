0a25cd8a67a0a2bd0ea097300d751a51366e7aaf
Amogh Krishna <amoghkrishna55@gmail.com>
2023-10-06 15:51:55 +0530

    lecture2

---
+++ lecture2.cpp
@@ -0,0 +1,7 @@
+#include<iostream> //This is a Header
+using namespace std;     // std makes sure that cout is a part of "iostream"
+
+int main(){      //We have written main function
+    cout << "Hello world";      // This will print out hello world in user screen
+    return 0;
+}
